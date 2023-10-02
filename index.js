ba2f5164f4002be41c9ed9feaa7f0deb4d60f32e
Amanda <amandalill@gmail.com>
2023-10-02 12:56:09 +0200

    better js code

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 
 
-let homeScore; 
-let guestScore;
+let homeScore = 0; 
+let guestScore = 0;
 let homeScoreBoard = document.getElementById("homeScore");
 let guestScoreBoard = document.getElementById("guestScore");
 
-function add1(points, team){
-    console.log("button clicked")
-    if(team === "home"){
-        homeScore += points;
-        homeScoreBoard.textContent = homeScore;
-    }
-    if(team === "guest"){
-        guestScore += points;
-        guestScoreBoard.textContent = guestScore;
-    }
-    else{
-        console.log("Error, scoring team not defined");
-    }
+const button = document.getElementsByClassName('scoreBtn');
+
+for(let i = 0; i<button.length; i++){
+    button[i].addEventListener('click', function(e){
+            
+        let points = parseInt(e.target.id.match(/\d/)); 
+
+        if(e.target.id.includes("home")){
+            homeScore += points; 
+            homeScoreBoard.textContent = homeScore;
+        }if(e.target.id.includes("guest")){
+            guestScore += points;
+            guestScoreBoard.textContent = guestScore;
+        }else{
+            console.log("error, team not defined")
+        }
+    });
 }
+
+
+
+
+
+// function add(points, team){
+    
+//     console.log("button clicked")
+
+//     if(team == "home"){
+//         homeScore += points;
+//         homeScoreBoard.textContent = homeScore;
+//     }
+//     if(team == "guest"){
+//         guestScore += points;
+//         guestScoreBoard.textContent = guestScore;
+//     }
+//     else{
+//         console.log("Error, scoring team not defined");
+//     }
+// }
